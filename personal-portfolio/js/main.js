83c158d2df637be04c5e1f8e729a772e278254eb
Aaron <aarrron.tran@gmail.com>
2023-07-04 20:41:03 +1000

    Changed Theme and Animations
    - added entry animations to most elements
    - modified animations to occur when scrolling and the element becomes visible
    - included flexbox
    
    TODO:
    - add resume
    - add projects section

--- personal-portfolio/js/main.js
+++ personal-portfolio/js/main.js
@@ -1,9 +1,26 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progress-bar").style.width = scrolled + "%";
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("progress-bar").style.width = scrolled + "%";
 }
+
+// Scroll Animations
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry)
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		}
+		else {
+			entry.target.classList.remove('show');
+		}
+	})
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
+
